394c20cb1018fd384acf0445f93d6431e60d93d3
Seif Hossam <seif.hossam26092002@gmail.com>
2022-12-20 16:18:51 +0200

    lolsss

--- myapp/app.js
+++ myapp/app.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-//login page lol
+//login page
 app.get('/', function (req, res) {
   if(global.registered)
     res.render('login',{msg:'You have registered successfully'});
