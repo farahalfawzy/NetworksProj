44843181015a0fbbcfa9cb3f738421b6b5f0603c
Seif Hossam <seif.hossam26092002@gmail.com>
2022-12-20 16:15:53 +0200

    commit

--- myapp/app.js
+++ myapp/app.js
@@ -1,70 +1,76 @@
+const { Console } = require('console');
 var express = require('express');
 var path = require('path');
-var fs=require('fs');
-
 var app = express();
-
+global.registered=false;
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/',function(req,res){
-    res.render('login');
+
+//login page
+app.get('/', function (req, res) {
+  if(global.registered)
+    res.render('login',{msg:'You have registered successfully'});
+  else
+    res.render('login',{msg:''});
 });
-app.get('/home',function (req,res){
+
+
+
+//home page
+app.get('/home', function (req, res) {
   res.render('home');
-
-});
-app.post('/',function(req,res){
-  var x =req.body.username
-  var y =req.body.password
-  console.log(x);
-  console.log(y);
-  res.render('home');
-
-});
-app.get('/registration',function (req,res){
-  res.render('registration');
-
 });
 
-// var x={name:"Ali",age:27,username:"ali2",password:"pass"};
-// var y=JSON.stringify(x);
-// fs.writeFileSync("users.json",y);
 
-// var data=fs.readFileSync("users.json");
-// var z= JSON.parse(data);
 
-app.post('/register',function(req,res){
-  console.log('here');
-  var x =req.body.username
-  var y =req.body.password
-  console.log(x);
-  console.log(y);
-  var MongoClient= require('mongodb').MongoClient;
-  MongoClient.connect("mongodb://127.0.0.1:27017",function(err,client){
+//registration page
+app.get('/registration', function (req, res) {
+  res.render('registration',{messageReg:""});
+});
+
+app.post('/register', function (req, res) {
+  var user = req.body.username;
+  var password = req.body.password;
+  let flag=false;
+  var MongoClient = require('mongodb').MongoClient;
+  MongoClient.connect("mongodb://127.0.0.1:27017/", function (err, client) {
     if (err) throw err;
-    var db=client.db("MyDB");
-    db.collection("FirstCollection").insertOne({id:1,username: x,password: y});
-    db.collection("FirstCollection").find().toArray(function(err,results){
-    console.log(results);
+    var db = client.db('MyDB');
+
+    db.collection('FirstCollection').find().toArray(function (err, items) {
+      if (err) throw err;
+      for (let i = 0; i < items.length; i++) {
+        if (items[i].Name == user){
+          flag=true;
+        }
+      }
+      if(user == "" || password == "")
+        res.render('registration',{messageReg:"You must enter a valid input!"});
+      if(flag && user != ""){
+        res.render('registration',{messageReg:"This user already exists"});
+      }
+      else{
+        if(user != "" && password !=0){
+          db.collection('FirstCollection').insertOne({ "Name": user, "password": password });
+          global.registered=true;
+          return res.redirect('/');
+        }
+      }
     });
   });
 });
-//search  Access MongoDV in Node.js
-
-const PORT = process.env.PORT || 3000;
 
 
-app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
-});
 
-//https://networks-abf9.onrender.com/
-
-
+if (process.env.PORT) {
+  app.listen(process.env.PORT, function () { console.log('Server started') });
+}
+else {
+  app.listen(3000, function () { console.log('Server started on port 3000') });
+}
